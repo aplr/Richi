22fe3625bdc46d617a5fa082373baff49b9085b1
Andreas Pfurtscheller <1051396+aplr@users.noreply.github.com>
2021-10-15 02:05:46 +0200

    Fix macos build (#3)
    Update CI to macos-11 and Xcode 13

---
+++ Sources/Richi/Internal/AppKit/CrossPlatform.swift
@@ -0,0 +1,14 @@
+//
+//  XP.swift
+//  Richi
+//
+//  Created by Andreas Pfurtscheller on 15.10.21.
+//
+
+#if canImport(AppKit)
+import AppKit
+/// :nodoc:
+public typealias UIView = NSView
+/// :nodoc:
+public typealias UIImage = NSImage
+#endif
